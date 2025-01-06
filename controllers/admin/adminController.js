80c647073683922f545a0631f77f7f8d2342a12f
Zafimahafeno <zanajaona2404@gmail.com>
2025-01-06 12:28:23 +0300

    bcrypt en bcyptjs

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,5 +1,5 @@
 const db = require('../../config/dbConfig');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');;
 
 const createAdmin = async (req, res) => {
   try {
--- controllers/client/etudiantController.js
+++ controllers/client/etudiantController.js
@@ -1,5 +1,5 @@
 const db = require('../../config/dbConfig');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');;
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
